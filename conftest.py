2dd894cb5f107a17a8b675e3ff7a95db79de6496
ПК <domniniv@mail.ru>
2022-12-05 11:32:44 +0400

    second commit

--- conftest.py
+++ conftest.py
@@ -4,6 +4,7 @@ from selene.support.shared import browser
 
 @pytest.fixture(scope="session")
 def br_set():
+    browser.config.window_width = 1024
+    browser.config.window_height = 768
     browser.open("https://demoqa.com/automation-practice-form")
-    browser.config.window_width = 800
-    browser.config.window_height = 600
+
