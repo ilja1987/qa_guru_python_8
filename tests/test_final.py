9f560458460e5f90acb1e089d60c0e6ef9c34c5f
ПК <domniniv@mail.ru>
2023-01-29 14:47:25 +0400

    -edits on comments

--- tests/test_final.py
+++ tests/test_final.py
@@ -3,10 +3,28 @@ from pages.practice_form import practice_form
 import allure
 from selene.support.shared import browser
 from utils import attach
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @allure.title('Тест на заполнение формы')
 def test_final(browser_setup):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "UNKNOWN",
+        "browserVersion": "",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="http://user1:1234@selenoid:4444/wd/hub",
+        options=options)
+
+    browser.config.driver=driver
+
     ilja = User(
         name='Andrew',
         last_name='Domnin',
