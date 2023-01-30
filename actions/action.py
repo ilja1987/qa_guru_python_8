4d992bc125fb8b2733e2b10453c5dee764fc4e51
ПК <domniniv@mail.ru>
2023-01-30 10:46:17 +0400

    -edits on comments

--- actions/action.py
+++ actions/action.py
@@ -1,16 +1,24 @@
 import os.path
 from selene.support.shared import browser
-from selene import have
+from selene import have, command
+
 
 def open_page():
     browser.open('/automation-practice-form')
+    ads = browser.all('[id^=google_ads_][id$=container__]')
+    ads.with_(timeout=10).should(have.size_greater_than_or_equal(3)).perform(command.js.remove)
+    google_uploadeds = browser.all('[id^=google_uploaded]')
+    if len(google_uploadeds) > 0:
+        google_uploadeds.with_(timeout=10).should(have.size_greater_than_or_equal(1)).perform(command.js.remove)
 
-def fill_user_fields(name,last_name,email,phone,adress):
+
+def fill_user_fields(name, last_name, email, phone, adress):
     browser.element('[id="firstName"]').type(name)
     browser.element('[id="lastName"]').type(last_name)
     browser.element('[id="userEmail"]').type(email)
     browser.element('[id="userNumber"]').type(phone)
     browser.element('#currentAddress').type(adress)
+
 
 def select_option(selector, text):
     browser.element(selector).click()
@@ -18,43 +26,53 @@ def select_option(selector, text):
         have.exact_text(text)
     ).click()
 
+
 def select_state(text):
-    select_option('#state',text)
+    select_option('#state', text)
+
 
 def select_sity(text):
     select_option('#city', text)
 
+
 def select_subjects(text):
     browser.element('[autocapitalize="none"]').type(text).press_tab()
+
 
 def upload_picture(path_to_photo):
     browser.element('#uploadPicture').set_value(
         os.path.abspath(
-            os.path.join(os.path.dirname(__file__), '../resources/'+ path_to_photo)
+            os.path.join(os.path.dirname(__file__), '../resources/' + path_to_photo)
         )
     )
 
-def select_date_of_birthday(day,month,year):
+
+def select_date_of_birthday(day, month, year):
     browser.element('[id="dateOfBirthInput"]').click()
     browser.element('.react-datepicker__month-select').click()
-    browser.element(f'[value="{month-1}"]').click()
+    browser.element(f'[value="{month - 1}"]').click()
     browser.element('.react-datepicker__year-select').click()
     browser.element(f'[value = "{year}"]').click()
     browser.element(f'.react-datepicker__day--00{day}').click()
 
+
 def push_submit_button():
     browser.element('[id="submit"]').press_enter()
 
-def asser_user_registration(name,email,male,phone,birthday,subjects,hobbies,photo,adress,options):
+
+def asser_user_registration(name, email, male, phone, birthday, subjects, hobbies, photo, adress, options):
     browser.all("tbody tr").should(have.size(10))
     browser.all('tbody tr td:last-child').should(have.texts(
-        name,email,male,phone,birthday,subjects,hobbies,photo,adress,options))
+        name, email, male, phone, birthday, subjects, hobbies, photo, adress, options))
+
 
 def select_gender(gender):
     browser.element(f'[name=gender][value={gender}]+label').click()
 
+
 def select_hobbies(hobies):
     checkboxes_click(browser.all('[for^=hobbies-checkbox]'), hobies)
+
 
 def checkboxes_click(elements, *by_texts):
     for value in by_texts:
--- tests/test_final.py
+++ tests/test_final.py
@@ -1,14 +1,10 @@
 from classes.user import User
 from pages.practice_form import practice_form
 import allure
-from selene.support.shared import browser
-from utils import attach
-
 
 
 @allure.title('Fill form test')
 def test_final():
-
     ilja = User(
         name='Andrew',
         last_name='Domnin',
@@ -27,11 +23,6 @@ def test_final():
         practice_form.fill(ilja)
     with allure.step('Test correct fill'):
         practice_form.assert_f(ilja)
-
-    attach.add_screenshot(browser)
-    attach.add_html(browser)
-    attach.add_logs(browser)
-    attach.add_video(browser)
 
 
 practice_form = practice_form()
